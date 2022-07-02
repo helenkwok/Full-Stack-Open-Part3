21169de3ca07cc489cd6a67b68b7a80486693104
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-03 01:08:00 +0930

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "id": 1,
@@ -48,6 +50,14 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  person.id = Math.floor(Math.random() * 1000000000)
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
