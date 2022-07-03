d9aa32442cad1decb94156db7fc215bc727248ed
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-07-03 23:53:49 +0930

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
 const express = require('express')
+const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
-const app = express()
-
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+app.use(express.json())
 
 app.use(cors())
+
+app.use(express.static('build'))
 
 morgan.token('body', function (request, response) { return JSON.stringify(request.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-app.use(express.json())
 
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br /> ${Date()}`)
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person
+  .find({})
+  .then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -96,14 +80,12 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-app.use(express.static('build'))
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
